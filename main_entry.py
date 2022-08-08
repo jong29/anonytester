8f64863f2a3d7b0983a8f459ab247a1bb472db23
jong29 <jongwoop97@gmail.com>
2022-08-08 14:33:07 +0900

    restored deleted main_entry file

---
+++ main_entry.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from classes.sidebar import SideBar
+
+class main_UI:
+    def __init__(self):
+        st.set_page_config(
+            page_title = "Anony Tester",
+            layout = "wide"
+        )   
+        st.image(".\logo\logo2.png", width=500)
+        SideBar()
+        
+if __name__ == '__main__':
+    main_UI()
+
