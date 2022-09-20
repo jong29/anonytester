499ec4922cd906399096568c9580a89a6a326720
user <jongwoop97@gmail.com>
2022-09-20 16:32:03 +0900

    attempt to fix not reidentifying

--- funcs/horiz_util.py
+++ funcs/horiz_util.py
@@ -6,7 +6,7 @@ import funcs.similarity as similarity
 
 import streamlit as st
 
-def process_chunk(chunk, raw_file):
+def process_chunk(chunk, raw_file, dims, record_num):
     # chunk = chunk.drop('an_usr_no', axis=1)
     prep_syn= preprocessing.preprocessing_syn(chunk)
     start = prep_syn.index[0]
@@ -17,27 +17,26 @@ def process_chunk(chunk, raw_file):
     prep_raw = preprocessing.preprocessing_raw_horiz(raw_chunk, start)
     
     # debugging
-    # st.write(f"start: {start}, end: {end}")
+    st.write(f"start: {start}, end: {end}")
     # st.write("Raw")
     # st.write(prep_raw)
     # st.write("Synthetic")
     # st.write(prep_syn)
     
+#     # 재식별도
+    syn_reidentified, _ = synthetic_reidentified.syn_reidentified_datas(\
+        prep_raw.copy(), prep_syn.copy(), K=record_num, start_dim=dims[0], end_dim=dims[1])
     
     # 재식별 위험도
     _, _, _, syn_table = risk_syn.compute_risk(prep_syn.copy())
-    print("위험도")
-    print(syn_table)
+    st.write("위험도")
+    st.write(syn_table)
     
 #     # 유사도
-    _, _, _, table_similarity = similarity.similarity(prep_raw, prep_syn, apply_hierarchy=False)
-    print("유사도")
-    print(table_similarity)
-    
-#     # 재식별도
-    syn_reidentified, _ = synthetic_reidentified.syn_reidentified_datas(\
-        prep_raw.copy(), prep_syn.copy(), K=-1, start_dim=1, end_dim=-1)
+    _, _, _, table_similarity = similarity.similarity(prep_raw.copy(), prep_syn.copy(), apply_hierarchy=False)
+    st.write("유사도")
+    st.write(table_similarity)
 
-    print("재식별 레코드")
-    print(syn_reidentified)
+    st.write("재식별 레코드")
+    st.write(syn_reidentified)
     
