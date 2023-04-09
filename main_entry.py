cec8aa6a03366a6e9773c677e36dd8d83010c382
user <jongwoop97@gmail.com>
2023-04-09 20:46:04 +0900

    fix path to logo using os

--- main_entry.py
+++ main_entry.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from classes.sidebar import SideBar
+import os
 
 class main_UI:
     def __init__(self):
         st.set_page_config(
             page_title = "Anony Tester",
             layout = "wide"
-        )   
-        st.image(r".\\logo\\logo2.png", width=500)
+        )
+        path = os.path.join(os.getcwd(), 'logo', 'logo2.png')   
+        print(path)
+        st.image(path, width=500)
         SideBar()
         
 if __name__ == '__main__':
